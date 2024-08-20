a22583c1865887b6cf810041ea218db2a6f9f074
krushna06 <krushnavyas06@gmail.com>
2024-08-20 11:02:38 +0530

    fix: ask for port

--- Networking Scripts/ssl.sh
+++ Networking Scripts/ssl.sh
@@ -5,13 +5,15 @@ sudo apt install -y certbot python3-certbot-nginx
 
 read -p "Enter your domain name (e.g., example.com): " domain_name
 
+read -p "Enter the port number your application is running on (e.g., 3001): " port_number
+
 sudo tee /etc/nginx/sites-available/$domain_name > /dev/null <<EOL
 server {
     listen 80;
     server_name $domain_name;
 
     location / {
-        proxy_pass http://localhost:3001;
+        proxy_pass http://localhost:$port_number;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
